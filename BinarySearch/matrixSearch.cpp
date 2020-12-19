5df97836a9db71795ce3cc0612e79a0e85d8f854
Pavan Reddy <37064259+Pavanreddy0@users.noreply.github.com>
2020-12-19 10:36:26 +0530

    matrix

---
+++ BinarySearch/matrixSearch.cpp
@@ -0,0 +1,15 @@
+
+int Solution::searchMatrix(vector<vector<int> > &A, int B) {
+    int m, n;
+    m = (int)A.size();
+    if(m == 0) return 0;
+    n = (int)A[0].size();
+    m = 0; n -= 1;
+    while(m < (int)A.size() && n >= 0)
+    {
+        if(A[m][n] == B) return 1;
+        else if(A[m][n] > B) n -= 1;
+        else m += 1;
+    }
+    return 0;
+}
